5c9aa71a385002662f0aa8f77d8d900616e08711
kal-aa <sadkalshayee@gmail.com>
2026-08-31 11:29:06 +0400

    use axios to fetch the data

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -1,21 +1,26 @@
 "use server";
 
+import axios from "axios";
 import Header from "../components/Header";
 import "./HomePage.css";
-import { useEffect, useState } from "react";
-// import { products } from "../../starting-code/data/products";
+// import { useEffect, useState } from "react";
+import { products } from "../../starting-code/data/products";
 
 export default function HomePage() {
-  const [products, setProducts] = useState([]);
+  // const [products, setProducts] = useState([]);
 
-  useEffect(() => {
-    async function fetchProducts() {
-      const response = await fetch("http://localhost:3000/api/products");
-      const data = await response.json();
-      setProducts(data);
-    }
+  // useEffect(() => {
+  //   async function fetchProducts() {
+  //     const response = await fetch("http://localhost:3000/api/products");
+  //     const data = await response.json();
+  //     setProducts(data);
+  //   }
 
-    fetchProducts();
+  //   fetchProducts();
+  // });
+
+  axios.get("http://localhost:3000/api/products").then((response) => {
+    console.log(response.data)
   });
 
   return (
