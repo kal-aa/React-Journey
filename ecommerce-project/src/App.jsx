7c0f6d99c4071eadefd1b25fa76fb9569b2e2145
kal-aa <sadkalshayee@gmail.com>
2026-09-04 20:31:16 +0400

    Add dayjs dependency and implement delivery options in CheckoutPage

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -20,7 +20,10 @@ function App() {
   return (
     <Routes>
       <Route index element={<HomePage cart={cart} />} />
-      <Route path="/checkout" element={<CheckoutPage cart={cart} />} />
+      <Route
+        path="/checkout"
+        element={<CheckoutPage setCart={setCart} cart={cart} />}
+      />
       <Route path="/orders" element={<OrdersPage />} />
       <Route path="/tracking" element={<TrackingPage />} />
       <Route path="*" element={<NotFoundPage />} />
