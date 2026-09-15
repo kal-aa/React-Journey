74f985823113fa991e9c277a6441b112a1c8e899
kal-aa <sadkalshayee@gmail.com>
2026-09-15 16:49:13 +0400

    Test clicking add to cart button

--- ecommerce-project/src/pages/home/Product.jsx
+++ ecommerce-project/src/pages/home/Product.jsx
@@ -70,7 +70,11 @@ export default function Product({ product, loadCart }) {
         Added
       </div>
 
-      <button className="add-to-cart-button button-primary" onClick={addToCart}>
+      <button
+        className="add-to-cart-button button-primary"
+        onClick={addToCart}
+        data-testid="add-to-cart-button"
+      >
         Add to Cart
       </button>
     </div>
--- ecommerce-project/src/pages/home/Product.test.jsx
+++ ecommerce-project/src/pages/home/Product.test.jsx
@@ -1,6 +1,10 @@
 import { describe, expect, it, vi } from "vitest";
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Product from "./Product";
+import axios from "axios";
+
+vi.mock("axios"); // mock the entire mock axios npm
 
 describe("Product component", () => {
   it("displays the product details correctly", () => {
@@ -32,5 +36,33 @@ describe("Product component", () => {
       ratingImgSrc,
     );
     expect(screen.getByText("87")).toBeInTheDocument();
+  });
+
+  it("Adds a product to the cart", async () => {
+    const product = {
+      id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+      image: "images/products/athletic-cotton-socks-6-pairs.jpg",
+      name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+      rating: {
+        stars: 4.5,
+        count: 87,
+      },
+      priceCents: 1090,
+      keywords: ["socks", "sports", "apparel"],
+    };
+
+    const loadCart = vi.fn();
+
+    render(<Product product={product} loadCart={loadCart} />);
+
+    const user = userEvent.setup();
+    const addToCartButton = screen.getByTestId("add-to-cart-button");
+    await user.click(addToCartButton);
+
+    expect(axios.post).toHaveBeenCalledWith("/api/cart-items", {
+      productId: product.id,
+      quantity: 1,
+    });
+    expect(loadCart).toHaveBeenCalled();
   });
 });
